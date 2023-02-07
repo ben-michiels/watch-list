9c448c54851511218e99bd85002232c075c27d59
Ben <ben.michiels@gmail.com>
2023-02-07 20:58:42 +0000

    Alter index.html meta and correct api address

--- src/api/Service.ts
+++ src/api/Service.ts
@@ -1,40 +1,40 @@
 import axios from "axios";
 
-const baseURL = "http://www.omdbapi.com/";
+const baseURL = "https://www.omdbapi.com/";
 
 const axiosInstance = axios.create({
-    baseURL,
-    params: {
-        apikey: "12777f22",
-    }
+  baseURL,
+  params: {
+    apikey: "12777f22",
+  },
 });
 
-axios.interceptors.response.use(
-    response => {
-        const { data } = response;
-        return data;
-    },
-);
+axios.interceptors.response.use((response) => {
+  const { data } = response;
+  return data;
+});
 
 export type EndpointParameters = Record<string, string | number>;
-export type EndpointFormatter = (endpointParameters: EndpointParameters) => string;
+export type EndpointFormatter = (
+  endpointParameters: EndpointParameters
+) => string;
 
 export default abstract class Service {
-    protected static readonly http = axiosInstance;
+  protected static readonly http = axiosInstance;
 
-    protected abstract readonly endpointFormatter: EndpointFormatter;
-    private endpointParameters: EndpointParameters;
+  protected abstract readonly endpointFormatter: EndpointFormatter;
+  private endpointParameters: EndpointParameters;
 
-    protected constructor() {
-        this.endpointParameters = {};
-    }
+  protected constructor() {
+    this.endpointParameters = {};
+  }
 
-    public using(parameters: EndpointParameters): this {
-        this.endpointParameters = parameters;
-        return this;
-    }
+  public using(parameters: EndpointParameters): this {
+    this.endpointParameters = parameters;
+    return this;
+  }
 
-    protected get endpoint(): string {
-        return this.endpointFormatter(this.endpointParameters);
-    }
+  protected get endpoint(): string {
+    return this.endpointFormatter(this.endpointParameters);
+  }
 }
